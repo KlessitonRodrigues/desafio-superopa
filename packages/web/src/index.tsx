7f24ea99da56fcc406f6301575fc2f156c2f1943
klessitonrds <klessitom@gmail.com>
2021-11-26 12:57:10 -0300

    added: home page

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>HELLO</h1>, document.getElementById('root'))
+import App from './app'
+
+ReactDOM.render(<App />, document.getElementById('root'))
