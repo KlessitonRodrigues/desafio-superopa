18cee5ed8fe0b58b42990ed58b741a759e5af073
klessitonrds <klessitom@gmail.com>
2021-11-26 13:34:33 -0300

    changed: api url

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -4,3 +4,6 @@ import ReactDOM from 'react-dom'
 import App from './app'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+// to test on mobile, ex: http://192.168.<PC-IP>:3000
+export const apiUrl = 'http://localhost:3000'
--- packages/web/src/pages/home/index.tsx
+++ packages/web/src/pages/home/index.tsx
@@ -1,14 +1,13 @@
 import React, { useReducer } from 'react'
 import axios from 'axios'
 
+import { apiUrl } from '../..'
 import Button from '../../templates/button'
 import Editor from '../../components/editor'
 import { initialState, reducer } from './store'
 import { Container, Header, ResponsiveRow } from './styled'
 
 type RestResponse = { success: boolean; msg?: string; data?: any }
-
-const url = 'http://localhost:3000'
 
 const Home = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
@@ -20,7 +19,7 @@ const Home = () => {
       onClick={async () => {
         try {
           const res = await axios.post<RestResponse>(
-            `${url}/api/${state.reqType}`,
+            `${apiUrl}/api/${state.reqType}`,
             JSON.parse(state.request)
           )
           res.data.success
